485acb5a3ec4fbd331a66363b12ab383f33abfc2
Nathan Martel <nathanmartel@gmail.com>
2020-04-30 23:57:15 -0700

    Controls skeleton cleanup

--- src/components/AppControls/AppControls.jsx
+++ src/components/AppControls/AppControls.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 
 const AppControls = () => {
   const [url, setUrl] = useState('');
-  const [reqType, setReqType] = useState('');
+  const [reqType, setReqType] = useState('GET');
   const [reqBody, setReqBody] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     console.log(`${reqType} | ${url} | ${reqBody} `);
   };
 
@@ -13,19 +14,25 @@ const AppControls = () => {
     <>
       <form onSubmit={handleSubmit}>
         <label>URL:
-          <input type="text" value={url} onChange={() => setUrl(e => e.target.value)} />
+          <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
         </label>
+        <br />
+
         <label>
-          <input type="radio" id="reqType-get" name="reqType" value="GET" checked={reqType === 'GET'} onChange={() => setReqType(e => e.target.value)}/>
+          <input type="radio" id="reqType-get" name="reqType" value="GET" checked={reqType === 'GET'} onChange={(e) => setReqType(e.target.value)}/>
           GET
         </label>
         <label>
-          <input type="radio" id="reqType-post" name="reqType" value="POST" checked={reqType === 'POST'} onChange={() => setReqType(e => e.target.value)} />
+          <input type="radio" id="reqType-post" name="reqType" value="POST" checked={reqType === 'POST'} onChange={(e) => setReqType(e.target.value)} />
           POST
         </label>
+        <br />
+
         <label>Body:
-          <input type="textarea" value={reqBody} onChange={() => setReqBody(e => e.target.value)} />
+          <textarea rows="10" value={reqBody} onChange={(e) => setReqBody(e.target.value)} />
         </label>
+        <br />
+
         <button type="submit">Submit</button>
       </form>
     </>
